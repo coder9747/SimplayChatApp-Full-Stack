a3719704a6d142716b752a603332cccdd6c3bbf7
Pratyush <pratyushkarn007@gmail.com>
2023-04-01 13:36:43 +0530

    finished Application

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -6,7 +6,6 @@ import ScrollToBottom from 'react-scroll-to-bottom';
 const socket = io.connect("http://localhost:4500");
 
 const Chat = () => {
-    const [online, setonline] = useState([""]);
     const [messages, setmessages] = useState([]);
     const { info } = useContext(Chatcontext);
     const [text, settext] = useState("");
@@ -15,67 +14,40 @@ const Chat = () => {
         let obj = {
             name: info.name,
             message: text,
-            roomId:info.roomId
+            roomId: info.roomId
         }
-        setmessages([...messages,obj]);
-        socket.emit("new-user-message",obj);
+        setmessages([...messages, obj]);
+        socket.emit("new-user-message", obj);
+        settext("")
+
 
     }
-    socket.on("message_recived",(data)=>
-    {
-        setmessages([...messages,data]);
+    socket.on("message_recived", (data) => {
+        setmessages([...messages, data]);
     })
-
-
-
-
-
-
-
-
-
-
     useEffect(() => {
         socket.emit("roomJoined", info);
-        socket.on("newuser", (data) => {
-            setonline([...online, data.name]);
-        })
-    },[])
+    }, [])
     return (
         <div className='main'>
             <h1>Live Chat</h1>
             <ScrollToBottom className="chatBody">
-                
-
-             
                 {
                     messages.map((item) => {
                         return (
-                            <div className={`${info.name === item.name?"mymessage":"usermessage"}`}>
+                            <div className={`${info.name === item.name ? "mymessage" : "usermessage"}`}>
                                 <div className="username">{item.name}</div>
                                 <div className="usertext">{item.message}</div>
                             </div>
                         )
                     })
                 }
-                
             </ScrollToBottom>
             <div className="input">
-                <input type="text" value={text} onChange={(e) => settext(e.target.value)} />
-                <button onClick={handleClick} onKeyDown={(e)=> (e.key === "Enter"?handleClick:null)}>
+                <input type="text" value={text} onKeyDown={(e)=> (e.key=="Enter"?handleClick:null)} onChange={(e) => settext(e.target.value)} />
+                <button onClick={handleClick}>
                     send
                 </button>
-            </div>
-            <div className="joineduser">
-                <h6 className='online'>Online
-                    <div>
-                        {
-                            online.map((name) => {
-                                return (<div>{name}</div>);
-                            })
-                        }
-                    </div>
-                </h6>
             </div>
 
         </div>
