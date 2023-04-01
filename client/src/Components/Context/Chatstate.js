84a3b1898d988faba8e469005c92b4cc7327b3a7
Pratyush <pratyushkarn007@gmail.com>
2023-04-01 10:01:00 +0530

    added Join component

--- client/src/Components/Context/Chatstate.js
+++ client/src/Components/Context/Chatstate.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Chatcontext from "./Chatcontext";
 
 const Chatstate = (props)=>
 {
-
+    const [info,setInfo] = useState({
+        name:"",
+        roomId:"",
+    })
     return(
-        <Chatcontext.Provider value={"hey"}>
+        <Chatcontext.Provider value={{info,setInfo}}>
             {props.children}
         </Chatcontext.Provider>
     )
--- client/src/Components/Join.js
+++ client/src/Components/Join.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Chatcontext from './Context/Chatcontext'
+import { useNavigate } from 'react-router-dom';
 
 const Join = () => {
-  return (
-    <div>
-      Join
-    </div>
-  )
+    const {info,setInfo}  = useContext(Chatcontext);
+    const navigate = useNavigate();
+    const handleClick = (e)=>
+    {
+        setInfo({...info,[e.target.name]:e.target.value});
+    }
+    const handleSubmit = (e)=>{
+        console.log(e);
+        if(info.name && info.roomId)
+        {
+            navigate("/chat");
+        }
+        else
+        {
+            e.preventDefault();
+        }
+        
+
+    }
+    return (
+        <div className='col-md-4 mx-auto mt-5'>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+                <input type="text" name='name' onChange={handleClick} value={info.name} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Room Id</label>
+                <input type="text" name='roomId' onChange={handleClick} value={info.roomId} className="form-control" id="exampleInputPassword1"/>
+            </div>
+            
+            <button onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
+        </div>
+    )
 }
 
 export default Join
